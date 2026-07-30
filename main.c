decec5706e34c6d3c63f625ca7a6c226e0ce8edc
Luthais Bradbury <68658961+internet-cabbage@users.noreply.github.com>
2026-07-31 00:18:13 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -9,22 +9,22 @@
 
 int main() {
     fprintf(stderr,"\nMax threads: %d \n", omp_get_max_threads());
+    omp_set_num_threads(10);
     // Seeding the random generation function, so it doesnt repeat values
-    srand(time(NULL));
+    srand((unsigned int)time(NULL));
     // Simulation parameters
-    int count = 0; // Number of bodies
-    int capacity = 9000; // Maximum number of bodies which the simulation can handle
-    int tSteps = 200;
-    double antiSingularity = 4.0;
+    size_t count = 0; // Number of bodies 
+    size_t capacity = 20000; // Maximum number of bodies which the simulation can handle
+    int tSteps = 2000;
+    double antiSingularity = 3.0;
     double G = 5;
     double theta = 1.0; 
-    int xMax = 10000; // Maximum x distance
+    int xMax = 10000; // Maximum x distances
     int yMax = 10000; // Self explanatory
     int zMax = 10000;
-    int vMax = 6; // Max velocity
     int mMax = 100; // Max mass
     int mMin = 90;
-    double dt = 0.04;
+    double dt = 0.15;
 
     double *xVals = calloc(capacity, sizeof(double));
     double *yVals = calloc(capacity, sizeof(double));
@@ -36,13 +36,13 @@ int main() {
 
     // Parameters for galaxy spawning
     vec3 xOff1 = {0.0, 0.0, 0.0}; vec3 vOff1 = {0.0, 0.0, 0.0};
-    vec3 xOff2 = {7000.0, 0.0, 0.0}; vec3 vOff2 = {-1.0, 0.0, 0.0};
+    vec3 xOff2 = {6000.0, 0.0, 0.0}; vec3 vOff2 = {-5.0, 0.0, 0.0};
 
-    // plummerFunc(int *capacity, int NSpawn, int count, double* xVals, double* yVals, double* zVals, double* vxVals, double* vyVals, double* vzVals, vec3 xOff, vec3 vOff, double aConst, double G, double* MVals) {
+    // plummerFunc(int *capacity, int NSpawn, int count, double* xVals, double* yVals, double* zVals, double* vxVals, double* vyVals, double* vzVals, vec3 xOff, vec3 vOff, double aConst, double G, double* MVals) { g
 
     double aConst1 = 1000.0;
-    count += plummerFunc(&capacity,6000,count,xVals,yVals,zVals,vxVals,vyVals,vzVals,xOff1,vOff1,aConst1,G,mVals, 6000);
-    count += distributionFunction(&capacity, 3000, count, 3000,-0.0015, 0, xVals, yVals, zVals, vxVals, vyVals, vzVals, xOff2, vOff2);
+    count += plummerFunc(&capacity,10000,count,xVals,yVals,zVals,vxVals,vyVals,vzVals,xOff1,vOff1,aConst1,G,mVals, 6000);
+    count += distributionFunction(&capacity, 10000, count, 3000,-0.005, 0, xVals, yVals, zVals, vxVals, vyVals, vzVals, xOff2, vOff2);
 
     //double *vxVals = randomContinuous(vMax, N);
     //double *vyVals = randomContinuous(vMax, N);
@@ -52,9 +52,10 @@ int main() {
     int framesPerWrite = 1;
 
     // Initialise bodies loop
+    int countInt = (int) count;
 
     body *bodies = calloc(count, sizeof(body));
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < countInt; i++) {
         vec3 position, velocity;
         position.x = xVals[i];
         position.y = yVals[i];
@@ -111,12 +112,12 @@ int main() {
     // Creating the tree
 
     // Inserts all bodies into the tree to populate it
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < countInt; i++) {
         insertBody(rootPtr, &bodies[i], bodies, antiSingularity);
     }
     // Uses the lovely tree to calculate the formaces and accelerations on all the bodies
     vec3 *accelArray = calloc(count, sizeof(vec3));
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < countInt; i++) {
         vec3 f = calculateForce(rootPtr, &bodies[i], antiSingularity, G, theta);
         accelArray[i].x = f.x / bodies[i].mass;
         accelArray[i].y = f.y / bodies[i].mass;
@@ -125,13 +126,13 @@ int main() {
 
     // Offsets the velocity to apply the leapfrog correction
 
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < countInt; i++) {
         bodies[i].velocity.x -= accelArray[i].x * (dt/2); 
         bodies[i].velocity.y -= accelArray[i].y * (dt/2); 
         bodies[i].velocity.z -= accelArray[i].z * (dt/2); 
     }
     fprintf(stderr, "Initial tree build successful. \n");
-    fprintf(stderr, "Node count after initial build: [%d / %d] \n", nodeCount, nodeCap);
+    fprintf(stderr, "Node count after initial build: [%zu / %zu] \n", nodeCount, nodeCap);
     
     // Now I free all the memory used to perform those calculations
 
@@ -148,6 +149,7 @@ int main() {
     // Checking if the file was actually created, as who knows what the C program would do otherwise
     if (dataFile == NULL) {
         fprintf(stderr ,"Output file can't be created:");
+        exit(1);
     }
     else {
         fprintf(stderr, "Output file successfully created. \n \nBeginning calculations: \n \n");
@@ -155,9 +157,11 @@ int main() {
 
     // Writes the parameters required for the PythonRenderer to interpret the data
 
-    int writesteps = tSteps / framesPerWrite;
-    fwrite(&count,sizeof(count),1,dataFile);
-    fwrite(&writesteps,sizeof(tSteps),1,dataFile);
+    int writeSteps = tSteps / framesPerWrite;
+    fwrite(&countInt,sizeof(countInt),1,dataFile);
+    fwrite(&writeSteps,sizeof(writeSteps),1,dataFile);
+
+    printf("Rendering parameters:\n-N: %d\n-tSteps: %d\n\n", countInt,writeSteps);
     //fwrite(&antiSingularity, sizeof(antiSingularity),1,dataFile);
 
     // The array storing the positions of all the bodies, to be written to a buffer
@@ -167,16 +171,15 @@ int main() {
     writeFrame(dataFile,bodies,count,frameBuffer);
 
     // Code for the loading bar
-    static char barString[] = "||||||||||||||||||||||||||||||||||||||||";
     static int barLength = 40;
 
     int barInterval = (tSteps / (11 * barLength));
-
+    if (barInterval < 1) { barInterval = 1;}
     for (int j = 0; j < tSteps; j++) {
         if ((j) % barInterval == 0) {
             printProgress(j, tSteps, barLength);
         }
-        timeLoop(bodies,count,antiSingularity,G,tSteps,dt,theta,xMax,yMax,zMax);
+        timeLoop(bodies,count,antiSingularity,G,dt,theta,xMax,yMax,zMax);
          /* And finally writing this output to a file, each row will contain the state for a single body at a single time
         [xPos_0, yPos_0, xPos_1, yPos_1...]
         */
